7e7e921c8782365f2710d966853811c8e9dc86a1
Etzio <enzo_002@hotmail.com>
2023-09-03 19:58:26 -0300

    conexion con bd local de msqli

--- main.py
+++ main.py
@@ -2,7 +2,37 @@ import sqlite3
 import pandas as pd
 import os
 
-os.getcwd()
+ruta = os.getcwd()
+
+path=(f"{ruta}\db_empresa.db")
+sqliteConnection = sqlite3.connect(path)
+cursor = sqliteConnection.cursor()
+
+#CODIGO PARA MIRAR TODAS LAS TABLAS CREADAS
+
+#cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+#print(cursor.fetchall())
+
+#vista de datos normal
+#query = 'SELECT  *FROM DEPARTAMENTO limit 2'
+#cursor.execute(query)
+#output = cursor.fetchall()
+#for row in output
+#   print(row)
+    
+#vista de datos con pandas
+query = "SELECT  *FROM DEPARTAMENTO limit 1"
+pd.read_sql_query(query, sqliteConnection).head(1)    
+    
+    
+
+
+
+
+
+
+
+
 
 
 
